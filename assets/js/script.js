d9fb229ef8aa1aacf0e10d3a6220a4a74c434e77
Syauqi <syauqi.d.u@gmail.com>
2024-10-27 19:14:58 +0700

    [FEAT] Menambahkan navbar dan halaman riwayat penjualan

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//Buat expand sidebar
+const hamBurger = document.querySelector('.toggle-btn');
+
+hamBurger.addEventListener('click', function () {
+  document.querySelector('#sidebar').classList.toggle('expand');
+});
+
