289cc487a544f713b6f4ca3abf2f768ade7efc36
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2024-10-28 04:27:23 +0700

    [FEAT] init empty pages inputDataPenjualan

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//Buat expand sidebar
+const hamBurger = document.querySelector('.toggle-btn');
+
+hamBurger.addEventListener('click', function () {
+  document.querySelector('#sidebar').classList.toggle('expand');
+});
