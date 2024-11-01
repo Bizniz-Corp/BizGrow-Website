157b7995b4af8d1e3ae5e94b4d20ebd5512c65dd
ahmadfadliakbar <afka.akbar@gmail.com>
2024-11-01 08:49:01 +0700

    Menambahkan input.js

---
+++ assets/js/input.js
@@ -0,0 +1,8 @@
+// Fungsi untuk menampilkan alert saat tombol ditekan
+function showAlert(type) {
+    if (type === 'manual') {
+        alert("Anda memilih Input Data Manual. Anda dapat melakukan input untuk setiap produk pada suatu tanggal.");
+    } else if (type === 'file') {
+        alert("Anda memilih Input Data File. Anda dapat melakukan input secara mudah dengan mengupload file dengan format .csv atau .xlsx .");
+    }
+}
