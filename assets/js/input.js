94f519cec556d67ecc7403b4ae1f3a5bc40ae0d2
ahmadfadliakbar <afka.akbar@gmail.com>
2024-11-08 10:15:28 +0700

    Update landing page

--- assets/js/input.js
+++
@@ -1,8 +0,0 @@
-// Fungsi untuk menampilkan alert saat tombol ditekan
-function showAlert(type) {
-    if (type === 'manual') {
-        alert("Anda memilih Input Data Manual. Anda dapat melakukan input untuk setiap produk pada suatu tanggal.");
-    } else if (type === 'file') {
-        alert("Anda memilih Input Data File. Anda dapat melakukan input secara mudah dengan mengupload file dengan format .csv atau .xlsx .");
-    }
-}
