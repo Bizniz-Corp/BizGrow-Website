2a6362b495afa4b02a6f2025cb65304d823384d3
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2024-11-08 09:48:54 +0700

    [FEAT] change json data

--- assets/js/komponen_prediksi.js
+++ assets/js/komponen_prediksi.js
@@ -1,55 +1,56 @@
-const ctx = document.getElementById('myChart').getContext('2d');
-let myChart;
-
-function updateChart(selectedProduct, jsonData) {
-    const productData = jsonData.find(item => item.produk === selectedProduct);
-
-    const config = {
-        type: 'line',
-        data: {
-            labels: productData.labels,
-            datasets: productData.datasets
+const config = {
+    type: 'line',
+    data: {
+        labels: [],  // akan diisi dari JSON
+        datasets: []
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top'
+            },
+            tooltip: {
+                mode: 'index',
+                intersect: false
+            }
         },
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: 'top',
-                },
-                tooltip: {
-                    mode: 'index',
-                    intersect: false,
+        interaction: {
+            mode: 'nearest',
+            axis: 'x',
+            intersect: false
+        },
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Bulan'
                 }
             },
-            interaction: {
-                mode: 'nearest',
-                axis: 'x',
-                intersect: false
-            },
-            scales: {
-                x: {
-                    title: {
-                        display: true,
-                        text: 'Bulan'
-                    }
-                },
-                y: {
-                    title: {
-                        display: true,
-                        text: 'Permintaan'
-                    }
+            y: {
+                title: {
+                    display: true,
+                    text: 'Permintaan'
                 }
             }
         }
-    };
-
-    if (myChart) {
-        myChart.destroy(); // Hapus chart lama jika ada
     }
-    myChart = new Chart(ctx, config);
+};
+
+const myChart = new Chart(document.getElementById('myChart'), config);
+
+function updateChart(productName, data) {
+    const productData = data.find(item => item.produk === productName);
+
+    if (productData) {
+        // Update labels dan dataset chart
+        myChart.data.labels = productData.labels;
+        myChart.data.datasets = productData.datasets;
+        myChart.update();
+    }
 }
 
-fetch('../data/prediksi_demand_tahun.json') // Path ke file JSON
+fetch('https://raw.githubusercontent.com/Bizniz-Corp/BizGrow-Website/refs/heads/1302223041_Syahreza/assets/data/prediksi_demand_tahun.json')
     .then(response => response.json())
     .then(jsonData => {
         const selectElement = document.getElementById('productSelect');
@@ -64,48 +65,6 @@ fetch('../data/prediksi_demand_tahun.json') // Path ke file JSON
     })
     .catch(error => console.error('Error loading data:', error));
 
-// // Konfigurasi chart
-// const config = {
-//     type: 'line',
-//     data: data,
-//     options: {
-//         responsive: true,
-//         plugins: {
-//             legend: {
-//                 position: 'top',
-//             },
-//             tooltip: {
-//                 mode: 'index',
-//                 intersect: false,
-//             }
-//         },
-//         interaction: {
-//             mode: 'nearest',
-//             axis: 'x',
-//             intersect: false
-//         },
-//         scales: {
-//             x: {
-//                 title: {
-//                     display: true,
-//                     text: 'Bulan'
-//                 }
-//             },
-//             y: {
-//                 title: {
-//                     display: true,
-//                     text: 'Permintaan'
-//                 }
-//             }
-//         }
-//     }
-// };
-
-// Menampilkan chart
-// const myChart = new Chart(
-//     document.getElementById('myChart'),
-//     config
-// );
 
 $(document).ready(function () {
     $('.search_select_box select').selectpicker();
